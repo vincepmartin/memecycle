d37f36fcf595dcba6a4ad15339cf088ba10325fe
adminvm <vmartin@fednat.com>
2020-08-09 18:04:44 -0700

    Adding data checking and reorg
    - Check to see if fields exist in data on fetch
    - Conditional rendering depending on loading etc.
    - Clean up of render code.
    - Added Date

--- src/components/RideView/RideView.js
+++ src/components/RideView/RideView.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import { Map, TileLayer, Polyline } from 'react-leaflet'
+import { Map, TileLayer, Polyline, Marker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { purple } from '@material-ui/core/colors'
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    color: 'purple'
+})
 
 // Convert the long/lat to numbers we can use.
 function getLongLatFloat(records) {
@@ -30,6 +40,10 @@ function RideView({rideID}) {
     const [rideData, setRideData] = React.useState()
 
 
+    // TODO: Come back and clean up the error handling. 
+    // a. How do we figure out if we have a proper data response?  Check each one?
+    // b. Do this all up front in bulk?
+
     React.useEffect(() => {
         fetch(`http://localhost:8080/rides/${rideID}`)
             .then(response => {
@@ -37,30 +51,46 @@ function RideView({rideID}) {
             })
             .then(ride => {
                 // Get the title.
-                setTitle(ride.title)
+                if(ride.title)
+                    setTitle(ride.title)
 
                 // Get description.
-                setDescription(ride.description)
+                if(ride.description) 
+                    setDescription(ride.description)
 
                 // Set ride data.
-                setRideData(JSON.parse(ride.rideData))
+                if(ride.rideData)
+                    setRideData(JSON.parse(ride.rideData))
                 
                 // Set download status
                 setDownload({loaded: true, error: null})  
             }).catch(error => {
                 setDownload({loaded: false, error: error})
             })            
-        }, [])
+    }, [])
 
-    return(
-        <Grid direction='column' container>
-            <Grid item>
-                <h1>{title}</h1>
+    // Render some stuff.
+    if(download.error) {
+        return(
+            <Grid direction='column' container>
+                <h1>Error!!</h1>
+                <p>{download.error}</p>
             </Grid>
-
-            <Grid item>
-                {download.error && <h3>{download.error}</h3>} 
-                {download.loaded && 
+        )
+    } else if(!download.loaded) {
+        return(
+            <Grid direction='column' container>
+                <h1>Loading ride...</h1>
+            </Grid>
+        )
+    } else {
+        return(
+            <Grid direction='column' container>
+                <Grid item>
+                    <h1>{title}</h1>
+                    <h3>{rideData.activity.timestamp}</h3>
+                </Grid>
+                <Grid item>
                     <Map style={{height: '300px', width:'auto'}} 
                         center={getCenter(getLongLatFloat(rideData.records))}
                         zoom={zoom}
@@ -72,23 +102,21 @@ function RideView({rideID}) {
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
                         <Polyline positions = {getLongLatFloat(rideData.records)}></Polyline>
+                        <Marker position = {getLongLatFloat(rideData.records)[0]}>Start</Marker>
+                        <Marker position = {getLongLatFloat(rideData.records)[getLongLatFloat(rideData.records).length - 1]}>Stop</Marker>
                     </Map>
-                }
-            </Grid> 
-            
-            {rideData && 
+                </Grid> 
                 <Grid container direction="row" justify="center" alignItems="center" spacing={5}>
                         <Grid item><h5>Distance: {rideData.sessions[0].total_distance}</h5></Grid>
                         <Grid item><h5>Feet Climbed: {rideData.sessions[0].total_ascent*1000}</h5></Grid>
                         <Grid item><h5>Feet Descended: {rideData.sessions[0].total_descent*1000}</h5></Grid>
                 </Grid>
-            }
-
-            <Grid item>
-                <p>{description}</p>
+                <Grid item>
+                    <p>{description}</p>
+                </Grid>
             </Grid>
-        </Grid>
-    )
+        )
+    }
 }
 
 export default RideView
